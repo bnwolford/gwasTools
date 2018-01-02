830e29919049b538ab348f8165db2af520699876
Brooke Wolford <bwolford@hunt.sph.umich.edu>
2018-01-02 16:06:44 -0500

    If p-values are 0 make them be the smallest precision value in R

--- ManhattanPlot.r
+++ ManhattanPlot.r
@@ -65,12 +65,14 @@ colLine <- c("red")
 gwas <- fread(opt$input)
 
 if(!opt$log10p) {
-	gwas$log10P <- -log10(gwas[[opt$pvalue]])
-	ycol <- "log10P"
+    gwas[[opt$pvalue]][which(gwas[[opt$pvalue]] == 0)] <- 2e-308 #if pvalue is 0 convert to smallest of R's precision
+    gwas$log10P <- -log10(gwas[[opt$pvalue]])
+    ycol <- "log10P"
 } else {
-	ycol <- opt$pvalue
+    ycol <- opt$pvalue
 }
 
+#subset to chr,pos,pvalue
 gwas <- na.omit(data.frame(gwas[,c(chrcol,poscol,ycol),with=F]))
 Nmarkers <- dim(gwas)[1]
 
