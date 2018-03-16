0c3afb1b8be482eacdea6d9c397a1e40250a83b0
Brooke Wolford <bwolford@hunt.sph.umich.edu>
2018-03-16 01:25:39 -0400

    Not really sure what I changed other than formatting of lines and indentations

--- ManhattanPlot.r
+++ ManhattanPlot.r
@@ -66,15 +66,15 @@ opt <- args$options
 print(opt)
 
 if (opt$hitregion != ""){
-	candidateRegions <- read.table(opt$hitregion,sep="\t",header=T,check.names=F,comment.char="")
+    candidateRegions <- read.table(opt$hitregion,sep="\t",header=T,check.names=F,comment.char="")
 } else {
-	candidateRegions <- data.frame(
-		'CHROM'=character(0),
-		'START'=numeric(0),
-		'END'=numeric(0),
-		'COL'=character(0),
-		'LEGENDTEXT'=character(0)
-		)
+    candidateRegions <- data.frame(
+        'CHROM'=character(0),
+        'START'=numeric(0),
+        'END'=numeric(0),
+        'COL'=character(0),
+        'LEGENDTEXT'=character(0)
+    )
 }
 
 chrcol <- opt$chr
@@ -148,8 +148,7 @@ prethin2 <- prethin2[order(rnorm(length(prethin2)))[1:(length(prethin2)/20)]]
 prethin <- c(prethin1,prethin2)
 
 # Additional thinning using unique after rounding position and p-value
-thinned <- prethin[which(!duplicated(data.frame(gwas[[chrcol]][prethin],
-				round(gwas[[poscol]][prethin]/10000),round(gwas[[ycol]][prethin],1))))]
+thinned <- prethin[which(!duplicated(data.frame(gwas[[chrcol]][prethin], round(gwas[[poscol]][prethin]/10000),round(gwas[[ycol]][prethin],1))))]
 
 # Plot all SNPs with p < 1E-6
 keepTop <- which(gwas[[ycol]] > 6)
@@ -227,96 +226,95 @@ if (opt$pdf==TRUE) { #plot as pdf, default for height/width/point size are custo
 } else {
     png(filename = paste0(opt$prefix,"_Manhattan.png"), width = opt$width, height = opt$height, pointsize = opt$pointsize)
 }
-    par(mar=c(5.1,5.1,4.1,1.1),las=1)
-    x = plotdata$plotPos
-    y = plotdata$log10P
-    maxY <- max(y,na.rm=T)
+par(mar=c(5.1,5.1,4.1,1.1),las=1)
+x = plotdata$plotPos
+y = plotdata$log10P
+maxY <- max(y,na.rm=T)
 
-    # Version with two y axes
-    if(maxY > opt$break.top){
-        # Manhattan plot with two different y axis scales
+# Version with two y axes
+if(maxY > opt$break.top){
+                                        # Manhattan plot with two different y axis scales
 
         # set axis labels of both scales
-        lab1 <- pretty(c(0,opt$break.top),n=ceiling(12 * (1-opt$top.size)))
-        lab1 <- c(lab1[lab1 < opt$break.top],opt$break.top)
-        lab2 <- pretty(c(opt$break.top,maxY),n=max(3,floor(12 * opt$top.size)))
-        lab2 <- lab2[lab2 > max(lab1)]
-
+    lab1 <- pretty(c(0,opt$break.top),n=ceiling(12 * (1-opt$top.size)))
+    lab1 <- c(lab1[lab1 < opt$break.top],opt$break.top)
+    lab2 <- pretty(c(opt$break.top,maxY),n=max(3,floor(12 * opt$top.size)))
+    lab2 <- lab2[lab2 > max(lab1)]
+    
         # resulting range of top scale in bottom scale units
-        top.range = opt$break.top/(1 - opt$top.size) - opt$break.top
-        top.data = max(lab2)-opt$break.top
+    top.range = opt$break.top/(1 - opt$top.size) - opt$break.top
+    top.data = max(lab2)-opt$break.top
         # function to rescale the top part
-        rescale = function(y) {opt$break.top+(y-opt$break.top)/(top.data/top.range)}
+    rescale = function(y) {opt$break.top+(y-opt$break.top)/(top.data/top.range)}
 
         # plot bottom part / rescaled
-        plot(x[y<opt$break.top],y[y<opt$break.top],ylim=c(0,opt$break.top+top.range),axes=FALSE,
-            pch=plotdata$pch[y<opt$break.top], cex=0.9,cex.lab=1.5,cex.axis=1.5, xaxt="n",
-            col=plotdata$pcol[y<opt$break.top], ylab=expression(-log[10]*italic(P)), xlab="",bty="n",
-            main=gsub("_"," ",paste0(opt$maintitle,"\n",format(Nmarkers,big.mark=",",scientific=F),
-            " variants")))
+    plot(x[y<opt$break.top],y[y<opt$break.top],ylim=c(0,opt$break.top+top.range),axes=FALSE,
+         pch=plotdata$pch[y<opt$break.top], cex=0.9,cex.lab=1.5,cex.axis=1.5, xaxt="n",
+         col=plotdata$pcol[y<opt$break.top], ylab=expression(-log[10]*italic(P)), xlab="",bty="n",
+         main=gsub("_"," ",paste0(opt$maintitle,"\n",format(Nmarkers,big.mark=",",scientific=F)," variants")))
         # plot top part
         points(x[y>opt$break.top],rescale(y[y>opt$break.top]),pch=plotdata$pch[y>opt$break.top],
             col=plotdata$pcol[y>opt$break.top],cex=0.9)
 
         # plot highlighted regions
-        for(hcol in unique(plotdata$highlightColor)){
-            topDot <- plotdata[which(plotdata$highlightColor == hcol & y>opt$break.top),]
-            if(length(topDot)>0) {
-                points(topDot$plotPos,rescale(topDot$log10P),pch=20,col=hcol, cex=0.9)
-            }
-            bottomDot <- plotdata[which(plotdata$highlightColor == hcol & y<=opt$break.top),]
-            if(length(bottomDot)>0) {
-                points(bottomDot$plotPos,bottomDot$log10P,pch=20,col=hcol, cex=0.9)
-            }
+    for(hcol in unique(plotdata$highlightColor)){
+        topDot <- plotdata[which(plotdata$highlightColor == hcol & y>opt$break.top),]
+        if(length(topDot)>0) {
+            points(topDot$plotPos,rescale(topDot$log10P),pch=20,col=hcol, cex=0.9)
         }
-
-        # add axes and axis labels
-        axis(1,at=chrLab[seq(1,length(chrLab),by=2)],
-            labels=chrs[1:length(chrLab)][seq(1,length(chrLab),by=2)],
-            las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=2)
-        axis(1,at=chrLab[seq(2,length(chrLab),by=2)],
-            labels=chrs[1:length(chrLab)][seq(2,length(chrLab),by=2)],
-            las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=0)
-        axis(side=2,at=lab1,cex.axis=1.5,cex.lab=1.5)
-        axis(side=2,at=rescale(lab2),labels=lab2,cex.axis=1.5,cex.lab=1.5)
-
-        # plot axis breaks and indicate line of axis break
-        box()
-        axis.break(axis=2,breakpos=opt$break.top,style="zigzag",brw=0.02)
-        axis.break(axis=4,breakpos=opt$break.top,style="zigzag",brw=0.02)
-        abline(h=opt$break.top,lwd=1.5,lty=2,col="grey")
-        if(length(yLine)>0) {
-            for(rl in 1:length(yLine)){
-                if(yLine[rl] <= opt$break.top) {
-                    abline(h=yLine[rl],lwd=1.5,col=colLine[rl],lty=2)
-                } else {
-                    abline(h=rescale(yLine[rl]),lwd=1.5,col=colLine[rl],lty=2)
-                }
-            }
+        bottomDot <- plotdata[which(plotdata$highlightColor == hcol & y<=opt$break.top),]
+        if(length(bottomDot)>0) {
+            points(bottomDot$plotPos,bottomDot$log10P,pch=20,col=hcol, cex=0.9)
         }
-	# Version with one y axis / no break in axis
-    } else {
-        plot(x,y,xaxt="n",pch=plotdata$pch,cex=0.9,cex.lab=1.5,cex.axis=1.5,xaxt="n",
-            col=plotdata$pcol,ylab=expression(-log[10]*italic(P)),xlab="",bty="o",
-            main=gsub("_"," ",paste0(opt$maintitle,"\n",format(Nmarkers,big.mark=",",scientific=F),
-            	" variants")),
-            ylim=c(0,ceiling(max(maxY+1,yLine))))
-        axis(1,at=chrLab[seq(1,length(chrLab),by=2)],
-            labels=chrs[1:length(chrLab)][seq(1,length(chrLab),by=2)],
-            las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=2)
-        axis(1,at=chrLab[seq(2,length(chrLab),by=2)],
-            labels=chrs[1:length(chrLab)][seq(2,length(chrLab),by=2)],
-            las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=0)
-        # plot highlighted regions on top
-        for(hcol in unique(plotdata$highlightColor)){
-            extraDot <- plotdata[which(plotdata$highlightColor == hcol),]
-            points(extraDot$plotPos,extraDot$log10P,pch=20,col=hcol, cex=0.9)
-        }
-        # genome-wide significance linn
-        if(length(yLine)>0) abline(h=yLine,lwd=1.5,col=colLine,lty=2)
     }
 
+        # add axes and axis labels
+    axis(1,at=chrLab[seq(1,length(chrLab),by=2)],
+         labels=chrs[1:length(chrLab)][seq(1,length(chrLab),by=2)],
+         las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=2)
+    axis(1,at=chrLab[seq(2,length(chrLab),by=2)],
+         labels=chrs[1:length(chrLab)][seq(2,length(chrLab),by=2)],
+         las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=0)
+    axis(side=2,at=lab1,cex.axis=1.5,cex.lab=1.5)
+    axis(side=2,at=rescale(lab2),labels=lab2,cex.axis=1.5,cex.lab=1.5)
+    
+        # plot axis breaks and indicate line of axis break
+    box()
+    axis.break(axis=2,breakpos=opt$break.top,style="zigzag",brw=0.02)
+    axis.break(axis=4,breakpos=opt$break.top,style="zigzag",brw=0.02)
+    abline(h=opt$break.top,lwd=1.5,lty=2,col="grey")
+    if(length(yLine)>0) {
+        for(rl in 1:length(yLine)){
+            if(yLine[rl] <= opt$break.top) {
+                abline(h=yLine[rl],lwd=1.5,col=colLine[rl],lty=2)
+            } else {
+                abline(h=rescale(yLine[rl]),lwd=1.5,col=colLine[rl],lty=2)
+            }
+        }
+    }
+	# Version with one y axis / no break in axis
+} else {
+    plot(x,y,xaxt="n",pch=plotdata$pch,cex=0.9,cex.lab=1.5,cex.axis=1.5,xaxt="n",
+         col=plotdata$pcol,ylab=expression(-log[10]*italic(P)),xlab="",bty="o",
+         main=gsub("_"," ",paste0(opt$maintitle,"\n",format(Nmarkers,big.mark=",",scientific=F),
+                                  " variants")),
+         ylim=c(0,ceiling(max(maxY+1,yLine))))
+    axis(1,at=chrLab[seq(1,length(chrLab),by=2)],
+         labels=chrs[1:length(chrLab)][seq(1,length(chrLab),by=2)],
+         las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=2)
+    axis(1,at=chrLab[seq(2,length(chrLab),by=2)],
+         labels=chrs[1:length(chrLab)][seq(2,length(chrLab),by=2)],
+         las=1,tick=F,cex.axis=1.5,cex.lab=1.5,line=0)
+                                        # plot highlighted regions on top
+    for(hcol in unique(plotdata$highlightColor)){
+        extraDot <- plotdata[which(plotdata$highlightColor == hcol),]
+        points(extraDot$plotPos,extraDot$log10P,pch=20,col=hcol, cex=0.9)
+    }
+                                        # genome-wide significance linn
+    if(length(yLine)>0) abline(h=yLine,lwd=1.5,col=colLine,lty=2)
+}
+
 	# Add legend if candidate regions are present 
-    ltext <- unique(candidateRegions[,c("COL","LEGENDTEXT")])
-    if(dim(ltext)[1]>0) legend("topleft",legend=ltext$LEGENDTEXT,col=ltext$COL,pch=15,bty="n")
+ltext <- unique(candidateRegions[,c("COL","LEGENDTEXT")])
+if(dim(ltext)[1]>0) legend("topleft",legend=ltext$LEGENDTEXT,col=ltext$COL,pch=15,bty="n")
 dev.off()
